9e0d0833c3b78b42857f8db7c951c462e1656be9
ajithrt007 <ajithrt170@gmail.com>
2023-09-01 15:46:13 +0530

    campus page editted

--- src/app/campus/page.tsx
+++ src/app/campus/page.tsx
@@ -1,12 +1,10 @@
-'use client'
-import { useRouter } from 'next/navigation'
+import { redirect } from 'next/navigation'
 
 export default function Campus(){
-    const router = useRouter()
-    router.push('https://empty-toque-418.notion.site/MOCK-PLACEMENT-b7a1461b58a8430a8a7ed8cce33e6a85')
+    redirect('https://empty-toque-418.notion.site/MOCK-PLACEMENT-b7a1461b58a8430a8a7ed8cce33e6a85')
     return(
         <div className="flex flex-col gap-5 items-center justify-center w-screen h-screen">
-            <p className="text-xl font-semibold">Redirecting..</p>
+            <p className="text-xl font-semibold">Redirecting...</p>
         </div>
     )
 }
